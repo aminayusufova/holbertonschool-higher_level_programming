7b811e47c449a1872dfd312e2c3aad6859a604db
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-05-30 16:43:27 +0400

    commited

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+This is a module-level docstring.
+"""
 def add_integer(a, b=98):
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
