c58e95ba88a0741f7c3763e23b89693407c023a9
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-05-30 22:26:10 +0400

    updated

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,19 +3,6 @@
 This is a module-level docstring.
 """
 def add_integer(a, b=98):
-     """
-    Adds two integers and returns the result.
-    
-    Args:
-        a (int or float): The first number.
-        b (int or float): The second number, defaults to 98.
-    
-    Returns:
-        int: The sum of the two numbers, with floats cast to integers before addition.
-    
-    Raises:
-        TypeError: If either of a or b are not integers or floats.
-    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
