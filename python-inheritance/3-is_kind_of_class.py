d151cb4e944f3a768b0fec5048984e6b770d4d54
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-06-01 00:12:13 +0400

    Commited

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains the function is_same_class.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checks if an object is sort of a class
+        -> through inheritance
+    """
+    if not isinstance(a_class, type):
+        raise TypeError("a_class type must be 'type'")
+    if isinstance(obj, a_class) or issubclass(type(obj), a_class):
+        return True
+    return False
