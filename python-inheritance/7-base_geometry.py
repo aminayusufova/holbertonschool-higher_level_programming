0358dc43247b06679df9d7c36024b8a56e29d141
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-06-01 10:08:29 +0400

    Commited

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module defines a base class for geometric shapes.
+"""
+
+
+class BaseGeometry():
+    """for use with shapes. Super class.
+    """
+
+    def area(self):
+        """instance method to calculate area of shape
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates integer input
+        """
+        if type(value) != int:
+            raise TypeError(name + " must be an integer")
+        elif value <= 0:
+            raise ValueError(name + " must be greater than 0")
