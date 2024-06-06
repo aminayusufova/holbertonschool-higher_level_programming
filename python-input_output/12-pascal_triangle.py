af986d3f6fd7839e02e78834a6ce7aadc4bd3683
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-06-06 23:38:22 +0400

    updated

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -13,14 +13,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     res = []
-    l = []
+    temp = []
     for x in range(n):
         row = []
         for y in range(x + 1):
             if x == 0 or y == 0 or x == y:
                 row.append(1)
             else:
-                row.append(l[y] + l[y - 1])
-        l = row
+                row.append(temp[y] + temp[y - 1])
+        temp = row
         res.append(row)
     return res
