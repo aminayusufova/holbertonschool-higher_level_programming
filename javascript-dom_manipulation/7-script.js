d019a73d21594844a2d315045ef5dda88c2f0808
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-07-25 23:42:02 +0400

    commited

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+  const listMovies = document.getElementById('list_movies');
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const movies = data.results;
+
+      movies.forEach(movie => {
+        const listItem = document.createElement('li');
+        listItem.textContent = movie.title;
+        listMovies.appendChild(listItem);
+      });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+});
