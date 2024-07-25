95855711e1f5dc4b0919d75ad7fa7f299fa41048
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-07-25 23:36:39 +0400

    commited

--- javascript-dom_manipulation/7-script.js
+++
@@ -1,3 +0,0 @@
-$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
-  $('DIV#character').text(data.name);
-});
--- javascript-dom_manipulation/8-script.js
+++
@@ -1,3 +0,0 @@
-$.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
-  $('UL#list_movies').append(...data.results.map(movie => `<li>${movie.title}</li>`));
-});
